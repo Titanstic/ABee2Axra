c15c71c4829a0504f6c91783b274f8cf104d9f7e
Titanstic <mgminkhant301999@gmail.com>
2022-07-24 12:46:22 +0630

    finish edit and job page

---
+++ js/profile.js
@@ -0,0 +1,19 @@
+let jobCards = document.querySelectorAll(".job-card");
+let profileModal = document.querySelector('.profile-modal');
+
+jobCards.forEach((jobCard) => {
+    jobCard.addEventListener("click", function(e) {
+        console.log(e);
+        window.location.href = "./jobpost.html";
+    })
+});
+
+
+document.getElementById("edirprofile").addEventListener("click", function(e) {
+    e.preventDefault();
+    profileModal.classList.add("active");
+});
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    profileModal.classList.remove("active");
+});
