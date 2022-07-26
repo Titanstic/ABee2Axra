7190a5dc49eded7a9e4e35f850ade39e526f09c6
Titanstic <mgminkhant301999@gmail.com>
2022-07-26 19:25:26 +0630

    finish sign up page

--- js/signup.js
+++ js/signup.js
@@ -1,13 +1,50 @@
-// start first container
+// ---------------------start first container
+
+// start password eye icon 
+let passwordEyeSlash = document.querySelector(".passwordeyeslash"),
+    passwordEye = document.querySelector(".passwordEye"),
+    cpasswordEyeSlash = document.querySelector(".cpasswordeyeslash"),
+    cpasswordEye = document.querySelector(".cpasswordEye");
+
+let passowrdInput = document.getElementById("password"),
+    confirmInput = document.getElementById("confrirm");
+
+passwordEyeSlash.addEventListener("click", function() {
+    this.classList.remove("active");
+    passwordEye.classList.add("active");
+    passowrdInput.type = "text";
+});
+
+passwordEye.addEventListener("click", function() {
+    this.classList.remove("active");
+    passwordEyeSlash.classList.add("active");
+    passowrdInput.type = "password";
+});
+
+cpasswordEyeSlash.addEventListener("click", function() {
+    this.classList.remove("active");
+    cpasswordEye.classList.add("active");
+    confirmInput.type = "text";
+});
+
+cpasswordEye.addEventListener("click", function() {
+    this.classList.remove("active");
+    cpasswordEyeSlash.classList.add("active");
+    confirmInput.type = "password";
+});
+
+// end password eye icon 
+
 document.getElementById("first-btn").addEventListener("click", function(e) {
     e.preventDefault();
 
     document.querySelector(".firstcontainer").classList.remove("active");
     document.querySelector(".secondcontainer").classList.add("active");
 });
-// end first container
 
-// start second container 
+// ---------------------end first container
+
+// ---------------------start second container 
 let secondBtns = document.querySelectorAll(".seond-btn");
 
 secondBtns.forEach((secondBtn) => {
@@ -18,12 +55,12 @@ secondBtns.forEach((secondBtn) => {
         document.querySelector(".thirdcontainer").classList.add("active");
     })
 });
-// end second container
+// ---------------------end second container
 
 
-// start third conainer 
+// ---------------------start third conainer 
 document.querySelector(".third-btn").addEventListener("click", function(e) {
     e.preventDefault();
     window.location.href = "./profile.html";
 });
-// end third container
+// ---------------------end third container
