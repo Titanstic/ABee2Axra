2f517e7801e8f01d1bfc6854a9eb1b048f950387
Titanstic <mgminkhant301999@gmail.com>
2022-07-26 19:10:30 +0630

    finish sign up page

---
+++ js/signup.js
@@ -0,0 +1,29 @@
+// start first container
+document.getElementById("first-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(".firstcontainer").classList.remove("active");
+    document.querySelector(".secondcontainer").classList.add("active");
+});
+// end first container
+
+// start second container 
+let secondBtns = document.querySelectorAll(".seond-btn");
+
+secondBtns.forEach((secondBtn) => {
+    secondBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(".secondcontainer").classList.remove("active");
+        document.querySelector(".thirdcontainer").classList.add("active");
+    })
+});
+// end second container
+
+
+// start third conainer 
+document.querySelector(".third-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.location.href = "../index.html";
+});
+// end third container
